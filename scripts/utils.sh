eedf30ede329fb4de1bab7f727c4ebe0194282a3
billw <plumteabill@qq.com>
2022-07-27 11:32:21 +0800

    correct sgx device path of dcap

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -148,19 +148,19 @@ ensure_installed_sgx_driver()
     local ret=0
     if [ -L /dev/sgx/enclave ] && [ -L /dev/sgx/provision ] && [ -c /dev/sgx_enclave ] && [ -c /dev/sgx_provision ] && [ ! -c /dev/isgx ]; then
         SGX_DRIVER="dcap"
-        SGX_DEVICES=("/dev/sgx_enclave" "/dev/sgx_provision")
+        SGX_DEVICES=("/dev/sgx/enclave" "/dev/sgx/provision")
         info="Your device exists: /dev/sgx/enclave /dev/sgx/provision /dev/sgx_enclave /dev/sgx_provision is related to the DCAP driver"
     elif [ ! -L /dev/sgx/enclave ] && [ -L /dev/sgx/provision ] && [ -c /dev/sgx_enclave ] && [ -c /dev/sgx_provision ] && [ ! -c /dev/isgx ]; then
         SGX_DRIVER="dcap"
-        SGX_DEVICES=("/dev/sgx_enclave" "/dev/sgx_provision")
+        SGX_DEVICES=("/dev/sgx/enclave" "/dev/sgx/provision")
         info="Your device exists: /dev/sgx/provision /dev/sgx_enclave /dev/sgx_provision is related to the DCAP driver"
     elif [ ! -L /dev/sgx/enclave ] && [ ! -L /dev/sgx/provision ] && [ -c /dev/sgx_enclave ] && [ -c /dev/sgx_provision ] && [ ! -c /dev/isgx ]; then
         SGX_DRIVER="dcap"
-        SGX_DEVICES=("/dev/sgx_enclave" "/dev/sgx_provision")
+        SGX_DEVICES=("/dev/sgx/enclave" "/dev/sgx/provision")
         info="Your device exists: /dev/sgx_enclave /dev/sgx_provision is related to the DCAP driver"
     elif [ ! -L /dev/sgx/enclave ] && [ ! -L /dev/sgx/provision ] && [ ! -c /dev/sgx_enclave ] && [ -c /dev/sgx_provision ] && [ ! -c /dev/isgx ]; then
         SGX_DRIVER="dcap"
-        SGX_DEVICES=("/dev/sgx_provision")
+        SGX_DEVICES=("/dev/sgx/provision")
         info="Your device exists: /dev/sgx_provision is related to the DCAP driver"
     elif [ ! -L /dev/sgx/enclave ] && [ ! -L /dev/sgx/provision ] && [ ! -c /dev/sgx_enclave ] && [ ! -c /dev/sgx_provision ] && [ -c /dev/isgx ]; then
         SGX_DRIVER="isgx"

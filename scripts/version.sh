03c14d75a64efe8f7a66d3427cea98a5a08afe5b
Bill.W <plumteabill@qq.com>
2022-08-05 17:51:21 +0800

    support show chain and config-gen version

--- scripts/version.sh
+++ scripts/version.sh
@@ -13,15 +13,17 @@ inner_docker_version()
 {
     local cgen_image=(`docker images | grep '^\b'cesslab/config-gen'\b ' | grep 'latest'`)
     cgen_image=${cgen_image[2]}
+    local cgen_version=$(docker run --rm cesslab/config-gen version)
     printf "Docker images:\n"
-    printf "  Config-gen: ${cgen_image}\n"
+    printf "  Config-gen: ${cgen_version} ${cgen_image}\n"
 
     if [ x"$mode" == x"authority" ]; then
         local chain_image=(`docker images | grep '^\b'cesslab/cess-chain'\b ' | grep 'latest'`)
         chain_image=${chain_image[2]}
+        local chain_version=$(docker run --rm cesslab/cess-chain ./cess-node --version)
+        printf "  Chain: ${chain_version} ${chain_image}\n"
         local sche_image=(`docker images | grep '^\b'cesslab/cess-scheduler'\b ' | grep 'latest'`)
         sche_image=${sche_image[2]}
-        printf "  Chain: ${chain_image}\n"
         local sche_version=$(docker run --rm cesslab/cess-scheduler ./cess-scheduler version)
         printf "  Scheduler: ${sche_version} ${sche_image}\n"
     elif [ x"$mode" == x"storage" ]; then
@@ -32,6 +34,7 @@ inner_docker_version()
     elif [ x"$mode" == x"watcher" ]; then
         local chain_image=(`docker images | grep '^\b'cesslab/cess-chain'\b ' | grep 'latest'`)
         chain_image=${chain_image[2]}
-        printf "  Chain: ${chain_image}\n"
+        local chain_version=$(docker run --rm cesslab/cess-chain ./cess-node --version)
+        printf "  Chain: ${chain_version} ${chain_image}\n"
     fi
 }

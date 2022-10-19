5473035ecd3730150c1f1fda59346181062000b0
Bill.W <wyi.net@gmail.com>
2022-10-19 15:19:17 +0800

    optimize show version

--- scripts/version.sh
+++ scripts/version.sh
@@ -12,34 +12,24 @@ version()
 
 inner_docker_version()
 {
-    local cgen_image=(`docker images | grep '^\b'cesslab/config-gen'\b ' | grep 'latest'`)
-    cgen_image=${cgen_image[2]}
-    local cgen_version=$(docker run --rm cesslab/config-gen version)
     printf "Docker images:\n"
-    printf "  Config-gen: ${cgen_version} ${cgen_image}\n"
+    show_version "Config-gen" "cesslab/config-gen" "version"
 
+    show_version "Chain" "cesslab/cess-chain" "./cess-node --version"
     if [ x"$mode" == x"authority" ]; then
-        local chain_image=(`docker images | grep '^\b'cesslab/cess-chain'\b ' | grep 'latest'`)
-        chain_image=${chain_image[2]}
-        local chain_version=$(docker run --rm cesslab/cess-chain ./cess-node --version)
-        printf "  Chain: ${chain_version} ${chain_image}\n"
-        local sche_image=(`docker images | grep '^\b'cesslab/cess-scheduler'\b ' | grep 'latest'`)
-        sche_image=${sche_image[2]}
-        local sche_version=$(docker run --rm cesslab/cess-scheduler ./cess-scheduler version)
-        printf "  Scheduler: ${sche_version} ${sche_image}\n"
+        show_version "Scheduler" "cesslab/cess-scheduler" "./cess-scheduler version"
     elif [ x"$mode" == x"storage" ]; then
-        local chain_image=(`docker images | grep '^\b'cesslab/cess-chain'\b ' | grep 'latest'`)
-        chain_image=${chain_image[2]}
-        local chain_version=$(docker run --rm cesslab/cess-chain ./cess-node --version)
-        printf "  Chain: ${chain_version} ${chain_image}\n"
-        local bucket_image=(`docker images | grep '^\b'cesslab/cess-bucket'\b ' | grep 'latest'`)
-        bucket_image=${bucket_image[2]}
-        local bucket_version=$(docker run --rm cesslab/cess-bucket ./cess-bucket version)
-        printf "  Bucket: $bucket_version ${bucket_image}\n"
-    elif [ x"$mode" == x"watcher" ]; then
-        local chain_image=(`docker images | grep '^\b'cesslab/cess-chain'\b ' | grep 'latest'`)
-        chain_image=${chain_image[2]}
-        local chain_version=$(docker run --rm cesslab/cess-chain ./cess-node --version)
-        printf "  Chain: ${chain_version} ${chain_image}\n"
+        show_version "Bucket" "cesslab/cess-bucket" "./cess-bucket version"
     fi
+}
+
+function show_version() {
+    local prog_name=$1
+    local image_name=$2
+    local image_tag=$default_image_tag
+    local version_cmd=$3
+    local image_hash=(`docker images | grep '^\b'$image_name'\b ' | grep $image_tag`)
+    image_hash=${image_hash[2]}
+    local version=$(docker run --rm $image_name:$image_tag $version_cmd)
+    printf "  $prog_name: ${version} ${image_hash}\n"
 }

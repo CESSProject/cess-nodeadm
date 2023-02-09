ddf42879e95e7279ff3b5a1e37778fe449af73f6
Bill.W <wyi.net@gmail.com>
2023-02-09 11:31:23 +0800

    remove image upgrade function(instead of watchtower)

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -54,45 +54,6 @@ function msg_color {
     fi
 }
 
-function upgrade_docker_image()
-{
-    local image_name=$1
-    local image_tag=$default_image_tag
-    if [ x"$2" != x"" ]; then
-        image_tag=$2
-    fi
-
-    local old_image=(`docker images | grep '^\b'cesslab/$image_name'\b ' | grep 'latest'`)
-    old_image=${old_image[2]}
-
-    local region="`yq eval ".node.region" $config_file`"
-    local docker_org="cesslab"
-    if [ x"$region" == x"cn" ]; then
-       docker_org=$aliyun_address/$docker_org
-    fi
-
-    local res=0
-    docker pull $docker_org/$image_name:$image_tag
-    res=$(($?|$res))
-    docker tag $docker_org/$image_name:$image_tag cesslab/$image_name
-
-    if [ $res -ne 0 ]; then
-        log_err "Download docker image $image_name:$image_tag failed"
-        return 1
-    fi
-
-    local new_image=(`docker images | grep '^\b'cesslab/$image_name'\b ' | grep 'latest'`)
-    new_image=${new_image[2]}
-    if [ x"$old_image" = x"$new_image" ]; then
-        log_info "The current docker image $image_name ($old_image) is already the latest"
-        return 1
-    fi
-    
-    log_info "The docker image of $image_name is changed from $old_image to $new_image"
-
-    return 0
-}
-
 check_port() {
     local port=$1
     local grep_port=`netstat -tlpn | grep "\b$port\b"`

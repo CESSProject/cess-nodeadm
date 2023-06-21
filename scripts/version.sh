5623c650acd343669b2adeef3be0701266b2ab58
Bill.w <wyi.net@gmail.com>
2023-06-21 18:41:10 +0800

    support kaleido services version printing

--- scripts/version.sh
+++ scripts/version.sh
@@ -17,7 +17,8 @@ inner_docker_version()
 
     show_version "Chain" "cesslab/cess-chain" "./cess-node --version"
     if [ x"$mode" == x"authority" ]; then
-        show_version "Scheduler" "cesslab/cess-scheduler" "./cess-scheduler version"
+        show_version "Kaleido" "cesslab/kaleido-rotator" "--version"
+        show_version "Kaleido" "cesslab/kaleido" "--version" "--entrypoint ./target/release/kaleido"
     elif [ x"$mode" == x"storage" ]; then
         show_version "Bucket" "cesslab/cess-bucket" "./cess-bucket version"
     fi
@@ -28,8 +29,9 @@ function show_version() {
     local image_name=$2
     local image_tag=$default_image_tag
     local version_cmd=$3
+    local extra_docker_opts=$4
     local image_hash=(`docker images | grep '^\b'$image_name'\b ' | grep $image_tag`)
     image_hash=${image_hash[2]}
-    local version=$(docker run --rm $image_name:$image_tag $version_cmd)
+    local version=$(docker run --rm $extra_docker_opts $image_name:$image_tag $version_cmd)
     printf "  $prog_name: ${version} ${image_hash}\n"
 }
